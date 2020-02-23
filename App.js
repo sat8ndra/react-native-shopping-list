8a4e20f26bb860e77e8773e98436ed95ae28fa4c
satyendra gupta <sat8ndra@gmail.com>
2020-02-24 01:47:56 +0530

    added image element

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, Image, StyleSheet} from 'react-native';
 
 const App = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Hello World</Text>
+      <Image
+        source={{uri: 'https://randomuser.me/api/portraits/men/1.jpg'}}
+        style={styles.img}
+      />
     </View>
   );
 };
@@ -18,6 +22,11 @@ const styles = StyleSheet.create({
   text: {
     color: 'darkslateblue',
     fontSize: 30,
+  },
+  img: {
+    width: 100,
+    height: 100,
+    borderRadius: 100 / 2,
   },
 });
 
