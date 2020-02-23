d1498159fa293bb5cd2f3b925e08f76ba1f47403
satyendra gupta <sat8ndra@gmail.com>
2020-02-24 01:13:26 +0530

    added inline style for text

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import {View, Text} from 'react-native';
 const App = () => {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Hello World</Text>
+      <Text style={{color: 'darkslateblue', fontSize: 30}}>Hello World</Text>
     </View>
   );
 };
