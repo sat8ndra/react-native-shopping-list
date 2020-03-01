c2e2ed47a0b51a22d508faea6b949288e9d20c0a
satyendra gupta <sat8ndra@gmail.com>
2020-03-02 03:15:14 +0530

    Added final AddItem button

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet, FlatList} from 'react-native';
+import {View, Text, StyleSheet, FlatList, Alert} from 'react-native';
 import Header from './components/Header';
+import ListItem from './components/ListItem';
+import AddItem from './components/AddItem';
 import {uuid} from 'uuidv4';
 
 const App = () => {
@@ -10,12 +12,32 @@ const App = () => {
     {id: uuid(), text: 'Bread'},
     {id: uuid(), text: 'Juice'},
   ]);
+
+  const deleteItem = id => {
+    setItems(prevItems => {
+      return prevItems.filter(item => item.id != id);
+    });
+  };
+
+  const addItem = text => {
+    if (!text) {
+      Alert.alert('Error', 'Please enter an item', {text: 'Ok'});
+    } else {
+      setItems(prevItems => {
+        return [{id: uuid(), text}, ...prevItems];
+      });
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Header title="Shopping Lists" />
+      <AddItem addItem={addItem} />
       <FlatList
         data={items}
-        renderItem={({item}) => <Text>{item.text}</Text>}
+        renderItem={({item}) => (
+          <ListItem item={item} deleteItem={deleteItem} />
+        )}
       />
     </View>
   );
